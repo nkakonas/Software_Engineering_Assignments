b55637685a5f002da035c899f94a29dcaab34ba3
Nikos Kakonas <t8190050@aueb.gr>
2022-04-19 04:20:02 +0300

    remove a library from import

--- seip2022_practical_assignments/gradeshistogram/src/main/java/gr/aueb/dmst/nkakonas/HistogramGenerator.java
+++ seip2022_practical_assignments/gradeshistogram/src/main/java/gr/aueb/dmst/nkakonas/HistogramGenerator.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
